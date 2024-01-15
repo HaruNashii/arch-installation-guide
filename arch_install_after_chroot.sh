cd512d42e21ef0abf04692c83e01d8592ab5e529
Haru <116776134+HaruNashii@users.noreply.github.com>
2024-01-15 15:19:42 -0300

    Update arch_install_after_chroot.sh

--- arch_install_after_chroot.sh
+++ arch_install_after_chroot.sh
@@ -1,6 +1,8 @@
-#!/bin/bash 
-
+#!/bin/bash
+ 
+#change the system settings of the timezone to Brazil Sao Paulo :)
 ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+#syncronize the timezone settings with the system
 hwclock --systohc
 
 clear 
@@ -9,23 +11,31 @@ date
 
 sleep 8
 clear
+#open the nano editor for you select your system language
 nano /etc/locale.gen
+#apply the locale.gen config in your system
 locale-gen
 
+#change the name of the host for "blake"
 hostnamectl set-hostname blake
 clear 
 echo "Create the Root Password"
 passwd
 
+#add permissions for your user "blake" to use varios commands and others things
 useradd -m -g users -G wheel,storage,power -s /bin/bash blake
+clear
 echo "Create the user Password"
 passwd blake
 
+#download useful packages that will help you and grub the system bootloader
 pacman -S os-prober network-manager-applet networkmanager grub efibootmgr
 
 clear 
 echo "Its Time My Friend, Its Time For The GRUB INSTALLLL"
 sleep 2 
 echo "so... that the fight begins"
+#try to install the grub on your machine using the ufi method
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch_grub --recheck
+#try to generate the grub config
 grub-mkconfig -o /boot/grub/grub.cfg
