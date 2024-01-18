05c2227efade8987fb4a1333629841e8e8aeaeea
Haru <parajogapaladins@gmail.com>
2024-01-18 19:32:42 -0300

    yep

--- arch_install.sh
+++ arch_install.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
+chmod a+x $PWD/arch_install_after_chroot.sh
 
 #convert the /dev/sda1 in to FAT32
 mkfs.fat -F32 /dev/sda1
 #convert the /dev/sda2 to be the swap
 mkswap /dev/sda2
-#convert the /dev/sda3 in to ext4
-mkfs.ext4 /dev/sda3 
-#convert the /dev/sda4 in to ext4
-mkfs.ext4 /dev/sda4
+
+#-add an argument for the while be a loop
+x=1
+clear 
+echo "What Filesystem Format You Will Use For '/' (/dev/sda3) and '/home' (/dev/sda4)"
+echo "1 = ext4, 2 = btrfs"
+read -p "asnwer : " answer
+while [ $x -le 2 ]; do
+
+	    case $answer in
+	        [1]*)
+		    #convert the /dev/sda3 in to ext4
+		    mkfs.ext4 /dev/sda3 
+		    #convert the /dev/sda4 in to ext4
+		    mkfs.ext4 /dev/sda4
+	            break
+	            ;;
+	        [2]*)
+		    #convert the /dev/sda3 in to btrfs
+		    mkfs.btrfs -f /dev/sda3 	
+		    #convert the /dev/sda4 in to btrfs
+		    mkfs.btrfs -f /dev/sda4
+	            break
+	            ;;
+	        *)
+		    echo "Invalid input. Please enter either '1' for (ext4) or '2' for (btrfs)."
+		    read -p "asnwer : " answer
+	            ;;
+	    esac
+done
+
 
 #mount the "/" of /dev/sda3 in /mnt
 mount /dev/sda3 /mnt 
-#create the home folder in the / folder of the /dev/sda3
-mkdir /mnt/home
 #create the boot folder in the / folder of the /dev/sda3
 mkdir /mnt/boot/efi
-#mount the "/home" of /dev/sda4 in /mnt
-mount /dev/sda4 /mnt/home
 #mount the "/boot" of /dev/sda1 in /mnt
 mount /dev/sda1 /mnt/boot/efi
+#create the home folder in the / folder of the /dev/sda3
+mkdir /mnt/home
+#mount the "/home" of /dev/sda4 in /mnt
+mount /dev/sda4 /mnt/home
 #turn on the swap
 swapon /dev/sda2
 
@@ -27,19 +55,28 @@ clear
 echo "check if everything is correctly mounted"
 lsblk
 sleep 6
-clear 
 
-# open the nano editor for you to check if the mirror's is correct
+
+clear 
+echo "check if the mirrorlist is correct"
+sleep 3
 nano /etc/pacman.d/mirrorlist
-# download all the necessary parts for the linux work
-pacstrap /mnt base base-devel linux linux-firmware nano vim dhcpcd
-#generate the config of the disks
+
+#install the base of linux and others useful tools in the /dev/sda3 the root of your system
+pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd
+
+#generate the config of all your disks
 genfstab -U -p /mnt >> /mnt/etc/fstab
+
 
 clear 
 echo "check if the fstab is correct (should have all /dev/sda(1,2,3,4)"
+sleep 2
+clear
 cat /mnt/etc/fstab
 sleep 5
+
+
 
 clear
 echo "Changing to the /dev/sda3/ disk, bye :)"
