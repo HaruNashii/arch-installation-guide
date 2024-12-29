8c5371e9d39c1feda92a71dc2708afb13eccb552
Haru <116776134+HaruNashii@users.noreply.github.com>
2024-12-29 01:45:52 -0300

    Update nvidia_sucks.sh

--- nvidia_sucks.sh
+++ nvidia_sucks.sh
@@ -1,5 +1,5 @@
 # install nvidia drivers
-sudo pacman -S --needed dkms nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader egl-wayland
+sudo pacman -S --needed dkms nvidia nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader egl-wayland
 
 # change the nvidia kernel module to support wayland
 # necessary to use (gnome on wayland/hyprland, etc)
@@ -9,9 +9,6 @@ sudo echo options nvidia_drm modeset=1 | sudo tee /etc/modprobe.d/nvidia_drm.con
 if ! [ -x "$(command -v gdm)" ]; then
 	sudo ln -s /dev/null /etc/udev/rules.d/61-gdm.rule
 fi
-
-# prevent the initramfs from containing the nouveau module making sure the kernel cannot load it during early boot.
-sudo sed -i 's/kms//g' /etc/mkinitcpio.conf
 
 # regenerate the kernel modules
 sudo mkinitcpio -p linux
