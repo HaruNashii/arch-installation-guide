7a5aa785621c43b9d2e2b5dbb20671573918ef0c
Haru <116776134+HaruNashii@users.noreply.github.com>
2024-12-29 02:18:34 -0300

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#add permission for the other scripts be executed
-chmod a+x $PWD/arch_install_after_chroot.sh
-chmod a+x $PWD/wifi_configuration.sh
-chmod a+x $PWD/nvidia_sucks.sh
+
 
 clear
 read -p "What Device Will Be Your Boot Partition? :" bootpartition
@@ -17,59 +14,34 @@ mkswap $swapdevice
 
 clear
 read -p "What Device Will Be Your Root Partition? :" rootpartition
+#convert the root partitiom in to ext4
+mkfs.ext4 $rootpartition
 
-
-#-add an argument for the while be a loop
-x=1
-clear 
-echo "What Filesystem Format You Will Use For The (Root Partition)"
-echo "1 = ext4, 2 = btrfs"
-read -p "asnwer : " answer
-while [ $x -le 2 ]; do
-
-	    case $answer in
-	        [1]*)
-		    #convert the root partitiom in to ext4
-		    mkfs.ext4 $rootpartition
-		    clear
-	            break
-	            ;;
-	        [2]*)
-		    #convert the root partition in to btrfs
-		    mkfs.btrfs -f $rootpartition
-		    clear
-	            break
-	            ;;
-	        *)
-		    echo "Invalid input. Please enter either '1' for (ext4) or '2' for (btrfs)."
-		    read -p "asnwer : " answer
-	            ;;
-	    esac
-done
-
-#mount the "/" in /mnt
+#mount the root partition in /mnt
 mount $rootpartition /mnt 
 
 #create the boot folder in the "/" folder
-mkdir /mnt/boot
-mkdir /mnt/boot/efi
+mkdir -p /mnt/boot/efi
 
-#mount the "boot" in /mnt/boot/efi
+#mount the boot partition in /mnt/boot/efi
 mount $bootpartition /mnt/boot/efi
 
 #turn on the swap
 swapon $swapdevice
 
-#install the base of linux and others useful tools in the root of your system
-pacstrap /mnt base linux linux-firmware nano
+#install the base of linux in the root of your system
+pacstrap /mnt base linux linux-firmware
 
-#generate the config of all your disks
+#generate the config file of all your disks
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-#move scripts do the mounted system
+#add permission for the other scripts be executed
+chmod a+x $PWD/arch_install_after_chroot.sh
+chmod a+x $PWD/nvidia_sucks.sh
+
+#copy scripts do the mounted system
 mkdir /mnt/arch-install-help-for-my-friend
 cp -rf $PWD/* /mnt/arch-install-help-for-my-friend/
-
 
 clear
 echo "Running Chroot..."
